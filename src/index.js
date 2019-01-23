b05800694cb9b87a85a74ecb96f6968c58196735
Brayn Brigoli <32309779+ilbrigz@users.noreply.github.com>
2019-01-23 19:58:34 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Main from "./Main";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <strike>yo</strike>
+      <Main />
     </div>
   );
 }
